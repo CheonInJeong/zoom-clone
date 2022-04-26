c5a0327a9110b4b657bf7a4f99084339f63e4bf3
chenchen <burub3@gmail.com>
2022-04-26 23:41:57 +0900

    add nickname

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,6 @@
+const msgList = document.querySelector("ul");
+const msgForm = document.querySelector("#message");
+const nickname = document.querySelector("#nick");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open", () => {
@@ -5,9 +8,31 @@ socket.addEventListener("open", () => {
 });
 
 socket.addEventListener("message", (message) => {
+  console.log(message);
   console.log(`from server : ${message.data}`);
+  const li = document.createElement("li");
+  li.innerHTML = message.data;
+  msgList.append(li);
 });
 
 socket.addEventListener("close", () => {
   console.log("websocket closed🙅🏻‍♀️");
 });
+
+nickname.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = nickname.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+});
+
+msgForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = msgForm.querySelector("input");
+  socket.send(makeMessage("message", input.value));
+  input.value = "";
+});
+
+let makeMessage = (type, payload) => {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+};
--- src/server.js
+++ src/server.js
@@ -16,12 +16,29 @@ app.get("/*", (req, res) => res.redirect("/"));
 //app.listen(3000, handleListen);
 const server = http.createServer(app); //http server
 const wss = new WebSocketServer.Server({ server }); //websocket server : http 서버 위에 ws서버를 올림
-
+const sockets = [];
 wss.on("connection", (socket) => {
-  console.log(socket);
+  sockets.push(socket);
+  socket["nickname"] = "Anonymous";
   socket.send("hello from sever");
   socket.on("close", () => {
     console.log("websocket closed from browser"); //브라우저에서 소켓 연결을 끊음.
   });
+  socket.on("message", (message) => {
+    console.log(JSON.parse(message));
+    let parsed = JSON.parse(message);
+    switch (parsed.type) {
+      case "message":
+        sockets.forEach((sk) => {
+          sk.send(`${socket.nickname} : ${parsed.payload}`);
+        });
+        break;
+      case "nickname":
+        console.log(parsed.payload);
+        socket["nickname"] = parsed.payload;
+        break;
+    }
+  });
 });
+
 server.listen(3000, handleListen);
